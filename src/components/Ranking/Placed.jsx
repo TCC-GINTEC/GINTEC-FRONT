9eb0bb9d220c5893cdb40f8be20b38f9581c8022
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-04-29 10:22:26 -0300

    Adicionando responsividade para tela de ranking

--- src/components/Ranking/Placed.jsx
+++ src/components/Ranking/Placed.jsx
@@ -7,10 +7,10 @@ export default function Placed({ users }) {
       {
         users.map((user, index) => {
           return (
-            <div className="flex flex-col gap-4 items-center ">
+            <div className="flex flex-col gap-5 items-center">
               <h2>{index + 1}º Lugar </h2>
               <Image alt="" src={user.img} height={50} width={50}></Image>
-              <p>{user.nome} do curso {user.sala} - {user.pontos} pontos</p>
+              <p className='w-4/5'>{user.nome} do curso {user.sala} : {user.pontos} pontos</p>
             </div>
           )
         })
