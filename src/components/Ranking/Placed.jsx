4d579692053b6f0c77a2af6191c338a36e30d98e
Nathan Silva <nathan.silva26@gmail.com>
2024-05-05 21:44:47 -0300

    fix

--- src/components/Ranking/Placed.jsx
+++ src/components/Ranking/Placed.jsx
@@ -7,7 +7,7 @@ export default function Placed({ users }) {
       {
         users.map((user, index) => {
           return (
-            <div className="flex flex-col gap-5 items-center">
+            <div key={index} className="flex flex-col gap-5 items-center">
               <h2>{index + 1}º Lugar </h2>
               <Image alt="" src={user.img} height={50} width={50}></Image>
               <p className='w-4/5'>{user.nome} do curso {user.sala} : {user.pontos} pontos</p>
