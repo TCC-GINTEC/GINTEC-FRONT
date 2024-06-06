35aee7c35526f53001dda6dda0f4e138fb1a0a3f
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-06-06 11:54:47 -0300

    Ajustes na cor dos inputs e label

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -46,38 +46,51 @@ export default function Login() {
 
     return (
         <div className="h-screen">
-            <div className='flex h-full'>
-                <img src="/images/backgroundLogin.png" className='hidden w-1/2 h-full object-cover sm:block' />
-                <section className='bg-gray-300 h-full w-full'>
+            <div className='flex h-full bg-[#dfeff2]'>
+                <div className='sm:w-3/4 flex justify-center items-center'>
+                            {/* <img src="/images/icon.svg" className='w-[200px] object-cover  hidden sm:block' /> */}
+                </div>
+                <section className=' bg-white h-full w-full sm:w-3/4 rounded-l-3xl'>
                     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                            <img className="mx-auto h-10 w-auto" src="/images/icon.svg" alt="Your Company" />
-                            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Logue na sua conta</h2>
+                            
+                            <h2 className="mt-10 text-center text-3xl font-semibold leading-9 tracking-tight text-black mb-7">Faça seu login</h2>
+                            <p className='text-2x 1 text-[#b7b7b7]'>Digite seu Email institucional e o seu RM nos campos abaixo e clique em entrar para fazer seu login</p>
                         </div>
 
                         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                             <form className="space-y-6" action="#" method='post' onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
                                 <div>
-                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">E-mail</label>
-                                    <div className="mt-2">
-                                        <input onChange={(e) => { setEmail(e.target.value) }} name="email" type="email" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    <div class="relative z-0">
+                                        <input onChange={(e) => { setEmail(e.target.value) }}name="email" type="email" iautoComplete="email" d="floating_standard" class="border-b-[#b7b7b7] block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#b7b7b7] focus:outline-none focus:ring-0 focus:border-[#b7b7b7] peer" placeholder=" " />
+                                        <label for="floating_standard" class="absolute text-sm font-medium text-[#b7b7b7] dark:text-[#cacaca] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#b7b7b7] peer-focus:dark:text-[#b7b7b7] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+                                            E-mail
+                                        </label>
                                     </div>
+                                    {/* <div className="mt-2">
+                                        <input onChange={(e) => { setEmail(e.target.value) }} name="email" type="email" autoComplete="email" className="block w-full rounded-md border-0 border-b border-b-black bg-transparent rounded-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" />
+                                    </div> */}
                                 </div>
 
                                 <div>
-                                    <div className="flex items-center justify-between">
-                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Senha</label>
-                                        <div className="text-sm">
-                                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Esqueceu a senha?</a>
+                                    <div className="flex  flex-col justify-between">
+                                        <div class="relative z-0">
+                                            <input onChange={(e) => { setPassword(e.target.value) }} type="password" iautoComplete="current-password" d="floating_standard" class="border-b-[#b7b7b7] block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-[#b7b7b7] focus:outline-none focus:ring-0 focus:border-[#b7b7b7] peer" placeholder=" " />
+                                            <label htmlFor="password" for="floating_standard" class="absolute text-sm font-medium text-[#b7b7b7] dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#b7b7b7] peer-focus:dark:text-[#b7b7b7] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+                                                Senha
+                                            </label>
                                         </div>
                                     </div>
-                                    <div className="mt-2">
-                                        <input onChange={(e) => { setPassword(e.target.value) }} name="password" type="password" autoComplete="current-password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    <div className="text-sm mt-5 ">
+                                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Esqueceu a senha?</a>
                                     </div>
+                                    {/* <div className="mt-2">
+                                        <input onChange={(e) => { setPassword(e.target.value) }} name="password" type="password" autoComplete="current-password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div> */}
                                 </div>
 
                                 <div>
-                                    <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Entrar</button>
+                                    <button type="submit" className="flex w-full justify-center rounded-full bg-[#005C6D] px-3 py-1.5 text-xl font-semibold leading-8 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 ">Entrar</button>
                                 </div>
                             </form>
                         </div>
