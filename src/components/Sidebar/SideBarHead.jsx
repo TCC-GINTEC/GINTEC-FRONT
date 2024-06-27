cbd58b32b6cffe4067aeaf1d51a442ee9b94485a
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-06-27 16:27:58 -0300

    Add cor de background no sidebar e logo

--- src/components/Sidebar/SideBarHead.jsx
+++ src/components/Sidebar/SideBarHead.jsx
@@ -3,8 +3,10 @@ export default function SideBarHead({ active, Action }) {
         <div className="flex items-center justify-between">
             <img className={`
             ${active ? 'block' : 'hidden'}
-          `}
-                src="/images/icon.svg" width={100} />
+            mt-4 ml-3
+            `}
+          
+                src="/images/logo.svg" width={130} />
             <button className="p-3 rounded-xl hover:bg-light-green" onClick={Action}>
                 <img className="h-8 w-8" src="https://cdn-icons-png.flaticon.com/512/7216/7216128.png"/>
             </button>
