ee5eacb4336eb5879f2a96a8166ceb965028bb85
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-03 20:27:53 -0300

    Add animação ao selecionar o tipo de cadastro

--- src/app/(logado)/cadastros/novos/page.jsx
+++ src/app/(logado)/cadastros/novos/page.jsx
@@ -7,12 +7,7 @@ export default function Novo() {
   const [show, setShow] = useState(false);
 
   function handleShow() {
-    if (show) {
-      setShow(false);
-    } else {
-
-      setShow(true);
-    }
+    setShow(prevShow => !prevShow); // Alternativa mais concisa para alternar entre true/false
   }
 
   return (
@@ -23,17 +18,16 @@ export default function Novo() {
         </Link>
         <h1 className='text-2xl font-medium'>Novo Cadastro</h1>
       </div>
-      <div className='flex flex-col gap-4 justify-center w-[525px] p-10 m-auto shadow-lg mt-10 text-[#666666]'>
+      <div className='flex flex-col gap-4 items-center w-[525px] p-10 m-auto shadow-lg mt-10 text-[#666666] rounded-xl'>
         <div onClick={handleShow} 
-          className='flex items-center justify-between w-3/4 h-[60px] px-6 text-lg rounded-full bg-[#E6EFF0]'>
+          className='flex items-center justify-between w-3/4 h-[60px] px-6 text-lg rounded-full bg-[#E6EFF0] cursor-pointer transition-colors duration-300 '>
           Tipo de cadastro
           <Icon icon="iconamoon:arrow-down-2-duotone" width={40} style={{ color: "#005261" }} />
         </div>
-        <div className='w-3/4 bg-[#E6EFF0] rounded-3xl' style={{ display: show ? 'block' : 'none' }}>
-          <ul className='space-y-4 p-6'>
+        <div className={`w-3/4 bg-[#E6EFF0] rounded-3xl overflow-hidden transition-all duration-500 ${show ? 'max-h-[300px] duration-300 p-6' : 'max-h-0 p-0'}`}>
+          <ul className='space-y-4'>
             <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white'><Link href='/cadastros/novos/patio'>Jogos de Pátio</Link></li>
             <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white'><Link href='/cadastros/novos/quadra'>Campeonatos de Quadra</Link></li>
-            <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white'><Link href='/cadastros/novos/patio'>Campeonatos de Pátio</Link></li>
             <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white'><Link href='/cadastros/novos/doacao'>Doações</Link></li>
           </ul>
         </div>
