94470184e6aabc4e92c36c787183e8d9ca29d91a
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-04 07:58:38 -0300

    Add navegação do botoão 'novo cadastro' para pagina cadastros/novos

--- src/app/(logado)/cadastros/page.jsx
+++ src/app/(logado)/cadastros/page.jsx
@@ -1,8 +1,10 @@
 "use client"
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { Icon } from '@iconify/react';
 
 export default function Cadastros(){
+  const router = useRouter()
   return(
     <div className='xl:w-2/4 flex flex-col justify-start m-auto'>
     <h1 className='text-3xl mb-10'>Cadastros</h1>
@@ -39,7 +41,7 @@ export default function Cadastros(){
           <p>Doações</p>
         </div>
       </section>  
-      <section class=" hidden sm:block relative w-[240px] h-[150px] border-4 rounded-lg border-dashed border-gray-600*:absolute *:w-full">
+      <section onClick={() => router.push('cadastros/novos')} className=" hidden sm:block relative w-[240px] h-[150px] border-4 rounded-lg border-dashed border-gray-600*:absolute *:w-full">
         <Icon icon={'ph:plus'} className='mt-5' width={50}></Icon>
         <div className="inset-5 flex  flex-col justify-center text-xl text-center font-semibold">
           <p>Novo Cadastro</p>
