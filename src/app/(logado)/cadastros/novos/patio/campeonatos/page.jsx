419662a1fa749a271ba9d6989e16b4e384a63683
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-05 11:57:47 -0300

    fix form de novos campeoanto de pátio na pagina novos/patio/campeonato

--- src/app/(logado)/cadastros/novos/patio/campeonatos/page.jsx
+++ src/app/(logado)/cadastros/novos/patio/campeonatos/page.jsx
@@ -13,7 +13,7 @@ export default function Campeonato() {
   
   const router = useRouter()
       
-  const handleCancelar = () => {
+  const handleBackPageNovos = () => {
     router.push('/cadastros/novos')
   }
 
@@ -25,7 +25,8 @@ export default function Campeonato() {
     const ponto = target.pontoCampeonato.value;
     const qntFase = target.qntFases.value;
     const data = target.dataCampeonato.value;
-
+    console.log(data)
+    
     setTimeout(() => {
       setModalOpen(true);
     }, 4000);
@@ -54,7 +55,31 @@ export default function Campeonato() {
           </div>
           <p className='text-[#005261] font-semibold'>Campeonato de Pátio</p>
         </div>
-        <CampeonatoPatioForm handleFormSubmit={handleFormSubmit} handleCloseForm={handleCancelar}/>
+
+        <form onSubmit={(e) => handleFormSubmit(e)} className='space-y-8 mt-10 w-3/4 flex items-center flex-col'>
+            <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+              Nome campeonato
+              <input type="text" name="nomeCampeonato" id="" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+              Pontuação do campeonato
+              <input type="number" name="pontoCampeonato" id="" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+              Quantidade de fases
+              <input type="number" name="qntFases" id="" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+              Data
+              <input type="date" name="dataCampeonato" id="" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <div className='flex sm:flex-row justify-evenly w-full'>
+              <button type='submit' className=' w-1/3  sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>Cadastrar</button>
+              <button onClick={() => handleBackPageNovos()} type='button' className=' w-1/3 sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>
+                cancelar
+              </button>
+            </div>
+          </form>
       </ContainerCampeonatoPatio>
       {isModalOpen && (
         <>
