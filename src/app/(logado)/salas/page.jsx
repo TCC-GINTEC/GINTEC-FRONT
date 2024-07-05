657ff994e98749149de51cb20ff0d6a839912edc
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-05 15:10:48 -0300

    fix pagina de organização de salas

--- src/app/(logado)/salas/page.jsx
+++ src/app/(logado)/salas/page.jsx
@@ -5,7 +5,7 @@ import { Icon } from '@iconify/react';
 
 export default function Salas() {
   const [linhas, setLinhas] = useState([]);
-  const [showFilterOptions, setShowFilterOptions] = useState(false); // Estado para controlar a exibição das opções de filtro
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
   const router = useRouter();
 
   const [retornoApi, setRetornoApi] = useState([
@@ -70,22 +70,22 @@ export default function Salas() {
   function handleFiltroCursos(e) {
     const filtroCurso = retornoApi.filter(element => element.curso === e.target.value);
     setLinhas(filtroCurso);
-    setShowFilterOptions(false); // Fechar as opções de filtro ao selecionar uma opção
+    setShowFilterOptions(false);
   }
 
   const uniqueCursos = [...new Set(retornoApi.map(data => data.curso))];
   
   return (
-    <>
-      <div className="relative left-4 w-[170px] ">
+    <div className='pl-[90px]'>
+      <div className="flex justify-end m-auto max-w-[920px] relative">
         <div
-          onClick={() => setShowFilterOptions(!showFilterOptions)} // Alternar estado para exibir/ocultar opções de filtro
-          className="bg-white p-1  rounded-3xl border-[3px] border-[#005261] my-4 cursor-pointer flex items-center"
+          onClick={() => setShowFilterOptions(!showFilterOptions)}
+          className="w-[170px] bg-white p-1 rounded-3xl border-[3px] border-[#005261] my-4 cursor-pointer flex items-center"
         >
           <Icon icon="mynaui:filter" className="mr-2 h-6 w-6" />filtrar
         </div>
         {showFilterOptions && (
-          <div className="absolute bg-white shadow-md rounded-lg mt-2 w-48 py-2">
+          <div className="absolute bg-white shadow-md rounded-lg mt-20 right-0  w-48 py-2">
             <ul>
               <li className="cursor-pointer hover:bg-gray-100 py-1 px-3" onClick={() => handleFiltroCursos({ target: { value: '' } })}>
                 Todos os cursos
@@ -96,22 +96,24 @@ export default function Salas() {
                 </li>
               ))}
             </ul>
-          </div>
-        )}
-      </div>
+         </div>
+         )}
+       </div>
 
-      <div className="w-3/4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mx-auto text-sm md:text-base">
+      <div className="max-w-[920px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto text-sm md:text-base">
         {linhas.length > 0 ? (
           linhas.map((data, index) => (
             <div key={index} className="w-full">
               <button 
-                className="h-[100px] w-full rounded-xl shadow-md flex justify-center items-center gap-4 p-4 md:p-9"
+                className="h-[100px] w-full rounded-xl shadow-md flex justify-evenly items-center gap-4 p-4 md:p-9"
                 onClick={() => router.push(`/salas/sala?curso=${data.curso}&serie=${data.serie}`)}
               >
-                <div className="w-12 h-12 bg-gray-500 rounded-full flex justify-center items-center">
-                  {/* Conteúdo dentro do círculo */}
+                <div className='flex justify-start'>
+                  <div className="w-12 h-12 bg-gray-500 rounded-full flex justify-center items-center">
+                    {/* Conteúdo dentro do círculo */}
+                  </div>
                 </div>
-                <h1 className={`${data.curso === 'recursos humanos' ? 'break-words' : ''}`}>
+                <h1 className={`${data.curso === 'recursos humanos' ? '' : ''}`}>
                   {data.serie}º {data.curso}
                 </h1>
               </button>
@@ -122,12 +124,14 @@ export default function Salas() {
             <div key={index} className="w-full">
               <button 
                 onClick={() => router.push(`/salas/sala?curso=${data.curso}&serie=${data.serie}`)}
-                className="h-[100px] w-full rounded-xl shadow-md flex justify-center items-center gap-4 p-4 md:p-9"
+                className="h-[100px] w-full rounded-xl shadow-md flex justify-evenly items-center gap-4 p-4 md:p-9"
               >
-                <div className="w-12 h-12 bg-gray-500 rounded-full flex justify-center items-center">
-                  {/* Conteúdo dentro do círculo */}
+                <div className='flex justify-center border border-red-500 w-1/3'>
+                  <div className="w-12 h-12 bg-gray-500 rounded-full flex justify-center items-center">
+                    {/* Conteúdo dentro do círculo */}
+                  </div>
                 </div>
-                <h1 className={`${data.curso === 'recursos humanos' ? 'break-words' : ''}`}>
+                <h1 className={`${data.curso === 'recursos humanos' ? '' : ''}`}>
                   {data.serie}º {data.curso}
                 </h1>
               </button>
@@ -135,6 +139,6 @@ export default function Salas() {
           ))
         )}
       </div>
-    </>
+    </div>
   );
 }
