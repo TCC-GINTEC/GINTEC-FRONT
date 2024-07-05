be439c41cd44061af9306cefded2415478a26586
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-05 12:16:56 -0300

    fix form de novos cadastros de campeonatos na pagina novos/quadra

--- src/app/(logado)/cadastros/novos/quadra/page.jsx
+++ src/app/(logado)/cadastros/novos/quadra/page.jsx
@@ -13,7 +13,7 @@ export default function Quadra() {
 
   const router = useRouter()
       
-  const handleCancelar = () => {
+  const handleBackPageNovos = () => {
     router.push('/cadastros/novos')
   }
 
@@ -53,7 +53,30 @@ export default function Quadra() {
           </div>
           <p className='text-[#005261] font-semibold'>Campeonato de Quadra</p>
         </div>
-        <QuadraForm handleFormSubmit={handleFormSubmit}  handleCloseForm={handleCancelar}/>
+        <form onSubmit={(e) => handleFormSubmit(e)} className='space-y-8 mt-10 w-3/4 flex items-center flex-col'>
+          <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+            Nome campeonato
+            <input type="text" name="nomeCampeonato"  className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+          </label>
+          <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+            Pontuação do campeonato
+            <input type="number" name="pontoCampeonato" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+          </label>
+          <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+            Quantidade de fases
+            <input type="number" name="qntFases" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+          </label>
+          <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+            Data
+            <input type="date" name="dataCampeonato" className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+          </label>
+          <div className='flex sm:flex-row sm:justify-evenly w-full'>
+              <button type='submit' className='w-1/3 sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>Cadastrar</button>
+              <button onClick={() => handleBackPageNovos()} type='button' className='w-1/3 sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>
+                cancelar
+              </button>
+          </div>
+        </form>
       </ContainerCampeonatoQuadra>
       {isModalOpen && (
         <>
