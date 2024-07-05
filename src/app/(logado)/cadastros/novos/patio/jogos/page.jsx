431e1033be02ed483654adacf4a70e8b7262d1b4
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-05 11:51:17 -0300

    fix form de novos jogos de pátio na pagina castros/patio/jogos

--- src/app/(logado)/cadastros/novos/patio/jogos/page.jsx
+++ src/app/(logado)/cadastros/novos/patio/jogos/page.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import {useRouter} from 'next/navigation'
 import Link from 'next/link';
 import { Icon } from '@iconify/react';
-import JogosPatioForm from '@/components/formCadastro/JogosPatioForm'; 
 import ContainerJogosPatio from '@/components/formCadastro/ContainerJogosPatio';
 import Modal from '@/components/formCadastro/modal'
 
@@ -18,17 +17,34 @@ export default function Patio() {
     router.push('/cadastros/novos')
   }
 
+  const handleExtraClick = () => {
+    setExtraClicked(!extraClicked);
+  };
+
+  const handlePontoExtra = (acao) => {
+    if (acao === 0) {
+      setPontoExtra((prev) => (prev === 0 ? 0 : prev - 50));
+    }
+    if (acao === 1) {
+      setPontoExtra((prev) => prev + 50);
+    }
+  };
+
+  const backPageNovos = () => {
+    router.push('/cadastros/novos');
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const target = e.target;
     const nome = target.nomeJogo.value;
     const data = target.dataJogo.value;
     const ponto = extraClicked ? (Number(target.pontoJogo.value) + Number(target.pontoExtra.value)) : Number(target.pontoJogo.value);
-    console.log(ponto)
     
     setTimeout(() => {
       setModalOpen(true);
     }, 4000);
+    
   };
 
   const closeModal = () => {
@@ -56,14 +72,57 @@ export default function Patio() {
           </div>
           <p className='text-[#005261] font-semibold'>Jogos de Patio</p>
         </div>
-        <JogosPatioForm 
-          handleFormSubmit={handleFormSubmit}
-           handleCloseForm={handleCancelar}
-           extraClicked={extraClicked}
-           setExtraClicked={setExtraClicked}
-           pontoExtra={pontoExtra}
-           setPontoExtra={setPontoExtra}
-           />
+        
+        <form onSubmit={handleFormSubmit} className='p-4 mt-10 w-full flex flex-col'>
+        <div className='flex sm:flex-row flex-col gap-4'>
+          <div className={`flex flex-col space-y-8 ${extraClicked ? 'w-full sm:w-1/2 transition-all duration-700' : 'w-full duration-700'}`}>
+            <input type="text" name='id' />
+            <label className='flex flex-col gap-3 w-full px-9 pt-2 h-[73px] rounded-2xl text-sm text-[#666666] bg-[#E6EFF0]'>
+              Nome do Jogo
+              <input type="text" name="nomeJogo"  className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex flex-col gap-2 w-full px-9 pt-2 h-[73px] rounded-2xl text-sm text-[#666666] bg-[#E6EFF0]'>
+              Pontuação do Jogo
+              <input type="text" name="pontoJogo"  className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex flex-col gap-3 w-full  px-9 pt-2 h-[73px] rounded-2xl text-sm text-[#666666] bg-[#E6EFF0]'>
+              Data
+              <input type="date" name="dataJogo"  className='bg-[#E6EFF0] text-[#005261] font-semibold text-lg' />
+            </label>
+            <label className='flex justify-between items-center gap-3 w-full px-9 pt-2 h-[70px] pb-2 rounded-2xl text-sm text-[#666666] bg-[#E6EFF0]'>
+              Pontuação do Extra
+              <div className="flex" onClick={handleExtraClick}>
+                <div className={`rounded-full pt-[5px] pb-[5px] pr-[5px] pl-[5px] w-[25px] h-[25px] border border-[#005261] ${extraClicked ? 'border-8' : ''}`}>
+                </div>
+              </div>
+            </label>
+          </div>
+          {extraClicked && (
+            <div className={`flex items-end ${extraClicked ? 'w-full sm:w-1/2 transition-all duration-1000' : ''} justify-center mb-10`}>
+              <div className='flex flex-col w-3/4 gap-2'>
+                <label className='flex flex-col gap-2 text-[#DADADA] border-b-4'>
+                  Adicionar pontos
+                  <input type="number" name='pontoExtra' className='text-[#005261] font-semibold text-lg' />
+                </label>
+                <div className='flex items-center justify-between'>
+                  <button type='button' onClick={() => handlePontoExtra(0)} className='bg-[#E6EFF0] p-2 rounded-full'>
+                    <Icon icon="subway:subtraction-1" style={{ color: "#666666" }} width={20} />
+                  </button>
+                  <button type='button' onClick={() => handlePontoExtra(1)} className='bg-[#E6EFF0] p-1 rounded-full'>
+                    <Icon icon="prime:plus" style={{ color: "#666666" }} width={30} />
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className='flex sm:flex-row sm:justify-evenly'>
+          <button type='submit' className='w-1/3 sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>Cadastrar</button>
+          <button onClick={backPageNovos} type='button' className='w-1/3 sm:mt-10 rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>
+            Cancelar
+          </button>
+        </div>
+      </form>
       </ContainerJogosPatio>
       {isModalOpen && (
         <>
