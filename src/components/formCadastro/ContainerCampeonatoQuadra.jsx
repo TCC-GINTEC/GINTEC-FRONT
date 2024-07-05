fb5108a562a2d0740f9f0bc70ad2f64fdd209583
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-05 15:26:32 -0300

    Add recado de instrução no forms nas paginas de edição

--- src/components/formCadastro/ContainerCampeonatoQuadra.jsx
+++ src/components/formCadastro/ContainerCampeonatoQuadra.jsx
@@ -1,7 +1,7 @@
 export default function ContainerCampeonatoPatio({children,alert,classe}){
   return(
     <div className={`mx-auto relative flex flex-col items-center justify-center max-w-[525px] p-x-4 pt-11 pb-4 border bg-white border-[#DADADA] rounded-3xl shadow-xl  ${classe}`}>
-    <h1 className='text-lg'>{alert}</h1>
+    <h1 className='text-lg z-50'>{alert}</h1>
     {children}
     </div>
   )
--- src/components/formCadastro/ContainerDoacao.jsx
+++ src/components/formCadastro/ContainerDoacao.jsx
@@ -1,7 +1,7 @@
 export default function ContainerDoacao({children,alert,classe}) {
     return(
       <div className={`mx-auto relative flex flex-col items-center justify-center max-w-[525px] px-4 pt-11 pb-4 bg-white border border-[#DADADA] rounded-3xl shadow-xl ${classe}`}>
-        <h1>{alert}</h1>
+        <h1 className='text-lg z-50'>{alert}</h1>
         {children}
       </div>
       )
