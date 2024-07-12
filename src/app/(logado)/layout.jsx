1993f6786a72aa479842ca01fb4b3d04b6bebf1c
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-12 12:17:49 -0300

    removendo o import do componente sidebar antigo no layout

--- src/app/(logado)/layout.jsx
+++ src/app/(logado)/layout.jsx
@@ -1,13 +1,10 @@
 "use client"
 
-import SideBar from "../../components/Sidebar/SideBar";
-
 export default function HomePage({ children }) {
   return (
     <html lang="pt-br">
       <body>
         <div className="flex min-h-screen w-full text-text-color">
-          <SideBar className="z-50 bg-gray-400" />
           <main className=" w-full py-3 px-3 ">
             {children}
           </main>
--- src/components/Sidebar/SideBarSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function SideBarSection({ children }) {
-    return (
-        <div className="p-4 w-full space-y-6">
-            {children}
-        </div>
-    )
-}
