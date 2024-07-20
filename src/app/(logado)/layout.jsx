f60d6e7328019303bcb5d84d63c23ba5f2bbf665
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-20 18:00:22 -0300

    ajuste na pagina layout

--- src/app/(logado)/layout.jsx
+++ src/app/(logado)/layout.jsx
@@ -1,14 +1,14 @@
 "use client"
 
-import {SideBar} from "@/components/Sidebar/SideBar"
+import SideBar from "../../components/Sidebar/SideBar";
 
 export default function HomePage({ children }) {
   return (
     <html lang="pt-br">
       <body>
         <div className="flex min-h-screen w-full text-text-color">
-          <SideBar className={""}/>
-          <main className=" w-full py-3 px-3 ">
+          <SideBar className="z-50  "/>
+          <main className=" w-full py-3 px-3 overflow-x-hidden ">
             {children}
           </main>
         </div>
@@ -16,3 +16,4 @@ export default function HomePage({ children }) {
     </html>
   );
 }
+
