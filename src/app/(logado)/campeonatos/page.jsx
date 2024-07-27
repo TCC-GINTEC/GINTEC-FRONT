073524eed3150abf33623b5ca13b5f3800a89714
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-27 12:58:51 -0300

    ajuste da responsividade da tela de campeonato

--- src/app/(logado)/campeonatos/page.jsx
+++ src/app/(logado)/campeonatos/page.jsx
@@ -10,6 +10,7 @@ export default function Campeonatos() {
   const [showFilterFasesOptions, setShowFilterFasesOptions] = useState(false);
   const [alertShowFase, setAlertShowFase] = useState(false);
   const [itens, setItens] = useState([]);
+  const [indexFiltroDia, setIndexFiltroDia] = useState(1)
 
   useEffect(() => {
     handleGetItens();
@@ -41,6 +42,30 @@ export default function Campeonatos() {
     setAlertShowFase(false);
   };
 
+  {/*largura da janela*/}
+
+  const [larguraJanela, setLarguraJanela] = useState(0);
+
+  useEffect(() => {
+      // Função para atualizar a largura da janela
+      const handleResize = () => {
+          setLarguraJanela(window.innerWidth);
+      };
+
+      // Configura a largura inicial
+      handleResize();
+
+      // Adiciona o listener para o evento resize
+      window.addEventListener('resize', handleResize);
+
+      // Cleanup: remove o listener quando o componente é desmontado
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+  }, []);
+
+  {/*largura do container */}
+
   const divRef = useRef(null);
   const [largura, setLargura] = useState(0);
 
@@ -55,6 +80,10 @@ export default function Campeonatos() {
 
     return () => window.removeEventListener('resize', updateWidth);
   }, []);
+
+  function informacoesDia(index){
+    setIndexFiltroDia(index)
+  }
 
   return (
     <>
@@ -114,29 +143,29 @@ export default function Campeonatos() {
       </div>
 
       <div ref={divRef}  className="w-[920px] max-w-full mx-auto">
-        <div className="flex flex-col items-center w-full sm:gap-6 sm:pr-10">
-          <div className="w-full sm:w-[626px] h-[195px] flex justify-evenly items-center shadow-2xl rounded-lg mx-auto">
-            <div className="w-[100px] sm:w-[118px] h-[138px] text-center flex flex-col justify-center rounded-lg bg-[#005261] text-[#FFFFFF]">
+        <div className="flex flex-col items-center w-full sm:gap-2 md:gap-6 sm:pr-10">
+          <div className={`${largura < 800?'w-[360px]':'sm:w-[626px]'} max-w-full h-[195px] flex justify-evenly items-center shadow-2xl rounded-lg mx-auto`}>
+            <div onClick={() => informacoesDia(1)} className={`${largura < 800 ? 'w-[100px]':' sm:w-[118px] '} h-[138px] text-center flex flex-col justify-center rounded-lg ${indexFiltroDia == 1?'bg-[#005261] text-[#FFFFFF]':'bg-[#F8F8F8] text-[#005261]'} transition-all duration-200`}>
               <h2 className="text-3xl font-medium">28</h2>
               <p>Ago</p>
             </div>
-            <div className="w-[100px] sm:w-[118px] h-[138px] text-center flex flex-col justify-center rounded-lg bg-[#F8F8F8] text-[#005261]">
+            <div  onClick={() => informacoesDia(2)}  className={`${largura < 800 ? 'w-[100px]':' sm:w-[118px] '} h-[138px] text-center flex flex-col justify-center rounded-lg ${indexFiltroDia == 2?'bg-[#005261] text-[#FFFFFF]':'bg-[#F8F8F8] text-[#005261]'} transition-all duration-200`}>
               <h2 className="text-3xl font-medium">29</h2>
               <p>Ago</p>
             </div>
-            <div className="w-[100px] sm:w-[118px] h-[138px] text-center flex flex-col justify-center rounded-lg bg-[#F8F8F8] text-[#005261]">
+            <div onClick={() => informacoesDia(3)} className={`${largura < 800 ? 'w-[100px]':' sm:w-[118px] '}  h-[138px] text-center flex flex-col justify-center rounded-lg ${indexFiltroDia == 3?'bg-[#005261] text-[#FFFFFF]':'bg-[#F8F8F8] text-[#005261]'} transition-all duration-200`}>
               <h2 className="text-3xl font-medium">30</h2>
               <p>Ago</p>
             </div>
           </div>
-          <div className={`mt-10 grid ${largura < 800 ? 'sm:grid-cols-2':' sm:grid-cols-3'} gap-8`}>
+          <div className={`mt-10 grid ${largura < 800 && largura >= 501 ? 'sm:grid-cols-2': largura < 500 && largura >= 300 ?'sm:grid-cols-1': ' sm:grid-cols-3'} ${larguraJanela < 720 && larguraJanela > 448?'grid-cols-2':larguraJanela < 448?'grid-cols-1':''} gap-8`}>
               {itens.map((item, index) => {
                 // Pegando o primeiro nome (ou palavra) do item
                 const imageName = item.text.split(' ')[0].toLowerCase().replace(/\s+/g, '-');
                 const encodedText = encodeURIComponent(item.text);
 
                 return (
-                  <div key={index} className="w-[257px] h-[119px] shadow-2xl rounded-lg flex items-center gap-4 pl-4" onClick={() => router.push(`/campeonatos/tabela?campeonato=${encodedText}`)}>
+                  <div key={index} className={`${larguraJanela < 600 && larguraJanela > 448 || largura < 700 && largura >= 501 ?'w-[200px]':' w-[275px]'}  h-[119px] shadow-2xl rounded-lg flex items-center gap-4 pl-4`} onClick={() => router.push(`/campeonatos/tabela?campeonato=${encodedText}`)}>
                     <div className="bg-[#9747FF] w-[49px] h-[51px] flex justify-center items-center rounded-lg">
                       <Image
                         src={`/images/img-${imageName}.svg`}
