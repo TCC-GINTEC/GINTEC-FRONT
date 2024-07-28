91e9d97dc4a55c40c79bc4c30aab9ac182180d43
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-07-28 14:16:05 -0300

    criando formulario para adicionar recados

--- src/app/(logado)/recados/page.jsx
+++ src/app/(logado)/recados/page.jsx
@@ -4,11 +4,25 @@ import { useRef, useEffect, useState } from 'react';
 import Calendar from "@/components/Calendar/Calendar"
 import { Icon } from '@iconify/react';
 import Image from 'next/image'
+import Modal from '@/components/formCadastro/modal'
+import ContainerDoacao from '@/components/formCadastro/ContainerDoacao';
 
 export default function Recados(){
-  const [mostrarCalendario, setMostrarCalendario] = useState(false)
-  const [periodoFiltro, setPeriodoFiltro] = useState(false);
+  const [modalSucessoOpen, setModalSucessoOpen] = useState(false);
 
+
+  const [mostrarCalendario, setMostrarCalendario] = useState(false)
+  const [mostrarFormulario, setMostrarFormulario] = useState(false)
+  const [opcaoForm, setOpcaoForm] = useState(0);
+  
+  const [todosRecadosFiltro, setTodosRecadosFiltro] = useState(false);
+  const [periodoFiltro, setPeriodoFiltro] = useState(false);
+  const [mostrarModal, setMostrarModal] = useState(false) 
+  const [mostrarModalOpcoes, setMostrarModalOpcoes] = useState(false);
+
+  function handleMostrarModal() {
+    setMostrarModalOpcoes(prevShow => !prevShow); // Alternativa mais concisa para alternar entre true/false
+  }
   const paletaCores = [
     { name: 'Prazos',  background: 'bg-[#FF4CA2]', img: 'img-prazo.svg' },
     { name: 'Organização de Sala', background: 'bg-[#005261]', img: 'organizacao-sala-icon.svg', coloricon:'text-white' },
@@ -18,7 +32,27 @@ export default function Recados(){
     { name: 'Oficinas', background: 'bg-[#00C1CF]',img:'img-oficina.svg' },
     { name: 'Campeonato de Pátio', background: 'bg-[#FF4C4D]',img:'img-domino.svg' }
   ];
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    let tituloRecado = target.titulo.value;
+    let visualizacao = target.visualizacao.value;
+    let data = target.diaMarcado.value;
+    let horario = target.horarioRecado.value;
+    let recado =  target.recado.value;
+    console.log(tituloRecado,visualizacao, data, horario,recado)
+    
+    setTimeout(() => {
+      setMostrarModal(false)
+      setModalSucessoOpen(true);      
+    }, 4000);
+  }
 
+  const closeModal = () => {
+    setMostrarFormulario(false)
+    setModalSucessoOpen(false);
+  }
+  
   const [larguraJanela, setLarguraJanela] = useState(0);
 
   useEffect(() => {
@@ -61,31 +95,40 @@ export default function Recados(){
   return (
     <div ref={divRef} className={`flex ${largura <= 1280 ? 'justify-end' : 'justify-end'} justify-center mt-8 relative`}>
     {/* div da esquerda */}
-      <div className={`mx-auto max-w-full ${largura < 800?'w-[400px] sm:mr-6':'w-[930px] sm:ml-10 md:ml-20 sm:mr-20'}  `}>
+      <div className={`mx-auto max-w-full ${largura < 800?'w-[430px] sm:mr-6':'w-[930px] sm:ml-10 md:ml-20 sm:mr-20'}  `}>
+       
         <div>
-          <h1 className='pl-3 text-3xl'>Recados</h1>
+          <div className='w-full flex justify-between'>
+            <h1 className='pl-3 text-3xl'>Recados</h1>
+            <div className={`${largura < 630 ? 'block' : 'hidden'}  w-[100px] flex justify-center mr-3`}>
+             <button className='w-[50px] h-[50px] bg-[#005261] grid place-content-center  rounded-2xl hover:scale-110 duration-150 transition-all' onClick={() => setMostrarCalendario(!mostrarCalendario)}>
+                 <Image src={'/images/img-calendario.svg'} className="text-white" width={25} height={24}/>
+             </button>
+            </div>
+          </div>
           {/* filtros */}
           <div className={`flex ${largura < 700   && largura >= 624?'md:flex-col justify-center':'sm:flex-row items-center'} border border-orange-500 mt-[50px] ml-4 mb-[50px] w-full`}>
             {/* filtro 1 */}
             <div className='relative flex justify-center'>
               <div
-                onClick={() => setPeriodoFiltro(!periodoFiltro)}
+                onClick={() => setTodosRecadosFiltro(!todosRecadosFiltro)}
                 className={` w-[179px] shadow-xl bg-white p-1 rounded-3xl border-[3px] border-[#005261] my-4 cursor-pointer flex items-center`}
               >
                 <p className='w-full p-1'>Todos os Recados</p>
                 <Icon
                   width={30}
                   icon="solar:alt-arrow-down-line-duotone"
-                  className={`text-black duration-300 transform ${periodoFiltro ? 'rotate-180' : 'rotate-0'}`}
+                  className={`text-black duration-300 transform ${todosRecadosFiltro ? 'rotate-180' : 'rotate-0'}`}
                 />
               </div>
-              {periodoFiltro && (
+              {todosRecadosFiltro && (
                 <>
-                  <div className="fixed inset-0 bg-black bg-opacity-20 z-50" onClick={() => setPeriodoFiltro(false)}></div>
+                  <div className="fixed inset-0 bg-black bg-opacity-20 z-50" onClick={() => setTodosRecadosFiltro(false)}></div>
                   <div className="absolute bg-white shadow-md rounded-lg mt-20 ml-20 w-48 py-2 z-50">
                     <ul>
-                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">1ª fase</li>
-                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">2ª fase</li>
+                      <li onClick={() => setTodosRecadosFiltro(!todosRecadosFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">Todos os recados</li>
+                      <li onClick={() => setTodosRecadosFiltro(!todosRecadosFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">Organização de sala</li>
+                      <li onClick={() => setTodosRecadosFiltro(!todosRecadosFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">Prazos</li>
                     </ul>
                   </div>
                 </>
@@ -110,8 +153,9 @@ export default function Recados(){
                   <div className="fixed inset-0 bg-black bg-opacity-20 z-50" onClick={() => setPeriodoFiltro(false)}></div>
                   <div className="absolute bg-white shadow-md rounded-lg mt-20 ml-20 w-48 py-2 z-50">
                     <ul>
-                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">1ª fase</li>
-                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">2ª fase</li>
+                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">Todos</li>
+                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">7 dias</li>
+                      <li onClick={() => setPeriodoFiltro(!periodoFiltro)} className="cursor-pointer hover:bg-gray-100 py-1 px-3">15 dias</li>
                     </ul>
                   </div>
                 </>
@@ -140,7 +184,7 @@ export default function Recados(){
       </div>
       {/* div da direita */}
       <div className={
-        ` ${largura < 639 ? `${mostrarCalendario?'block z-50 fixed right-0 top-8 ':'hidden'} ` : 'block'} ${largura < 1086 ? 'w-[320px] ' : larguraJanela < 916 ? ' pr-8' : 'w-[520px]'} 
+        ` ${largura < 639 ? `${mostrarCalendario?'block z-50 fixed right-0 top-8 ':'hidden'} ` : 'block'} ${largura < 1086 ? 'w-[350px] ' : larguraJanela < 916 ? ' pr-8' : 'w-[520px]'} 
         h-[690px] rounded-l-lg bg-[#efeeee] border border-green-500 `}>
         <Image src={'/images/fechar-menu.svg'} className={`relative -left-1 -top-3 ${largura>625?'hidden':'block'}`} width={40} height={30} onClick={() =>setMostrarCalendario(!mostrarCalendario)}/>
         
@@ -154,7 +198,7 @@ export default function Recados(){
             <hr className='border-[#005261] border-b-2'/>
           </section>
           {/* botao */}
-          <button className='mx-auto max-w-full w-[310px] h-[90px] rounded-2xl flex justify-center items-center gap-4 bg-white mt-10'>
+          <button onClick={() => setMostrarModal(!mostrarModal)} className='mx-auto max-w-full w-[310px] h-[90px] rounded-2xl flex justify-center items-center gap-4 bg-white mt-10'>
             <div className="w-[45px] h-[45px] rounded-lg bg-[#005261] grid place-content-center p-6">
               <Icon width={15} icon="el:plus" className="text-white" />
             </div>
@@ -162,13 +206,104 @@ export default function Recados(){
           </button>
         </div>
       </div>
+         {modalSucessoOpen && (
+            <>
+                 <div className="fixed inset-0 bg-black bg-opacity-50 z-40"></div> {/* Fundo preto translúcido */}
+                 <div className="fixed -top-8 inset-0 flex items-center justify-center z-50">
+                   <div className="mx-auto  w-[290px] h-[220] sm:w-[390px] sm:h-[330px] bg-white p-6 rounded-lg shadow-lg relative z-50">
+                      <image src=".././images/sucess-form.png" className='absolute -top-[43px] left-[53px] sm:-top-[43px] sm:left-20 h-[154px] w-[200px] sm:h-[159px] sm:w-[217px]' alt="" />
+                      <button className="absolute top-2 right-2 text-gray-400 hover:text-gray-600" onClick={closeModal}>
+                        ✕
+                      </button>
+                      <div className='mt-28 text-center'>
+                        <h3 className="font-bold text-2xl">Sucesso!</h3>
+                        <p className="py-4 text-2xl">a</p>
+                      </div>
+                  </div>
+                  </div>
+            </>
+          )}
+      {mostrarModal && (
+          <>
+            <div className="fixed inset-0 bg-black bg-opacity-50 z-40"></div> {/* Fundo preto translúcido */}
+            <div className="fixed -top-8 inset-0 flex items-center justify-center z-50">
+              <div className={`${mostrarFormulario? 'hidden': 'block'} flex flex-col gap-4 z-50 bg-white items-center w-[350px] max-w-full sm:w-[525px] p-10 m-auto shadow-lg mt-10 text-[#666666] rounded-xl relative`}>
+                <div className='absolute left-4 top-2 cursor-pointer' onClick={() => setMostrarModal(!mostrarModal)}>
+                   <Icon icon="solar:arrow-left-linear" style={{ color: "#005261" }} width={40} />
+                </div>
+                <div 
+                  onClick={handleMostrarModal}
+                  className='flex items-center justify-between w-full sm:w-3/4 h-[60px] px-6 text-lg rounded-full bg-[#E6EFF0] cursor-pointer transition-colors duration-300 z-60'> {/* Cabeçalho do modal */}
+                  Tipo de cadastro
+                  <Icon icon="iconamoon:arrow-down-2-duotone" width={40} style={{ color: "#005261" }} />
+                </div>
+                <div className={`sm:w-3/4 bg-[#E6EFF0] rounded-3xl overflow-hidden transition-all duration-500 z-60 ${mostrarModalOpcoes ? 'max-h-[400px] duration-300 p-6' : 'max-h-0 p-0'}`}>
+                  {/* Opções dentro do modal */}
+                  <ul className='space-y-4'>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer'  onClick={() =>{ setMostrarFormulario(!mostrarFormulario);  setOpcaoForm(1)}}>Organização de Sala</li>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer'  onClick={() => {setMostrarFormulario(!mostrarFormulario); setOpcaoForm(2)}}>Campeonato de Pátio</li>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer'  onClick={() => {setMostrarFormulario(!mostrarFormulario); setOpcaoForm(3)}}>Campeonato de Quadra</li>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer' onClick={() => { setMostrarFormulario(!mostrarFormulario);  setOpcaoForm(4)}}>Prazos</li>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer'  onClick={() => {setMostrarFormulario(!mostrarFormulario); setOpcaoForm(5)}}>Doações</li>
+                    <li className='hover:bg-[#005261] rounded-xl p-3 hover:text-white hover:cursor-pointer'  onClick={() => {setMostrarFormulario(!mostrarFormulario); setOpcaoForm(6)}}>Oficinas</li>
+                  </ul>
+                </div>
+              </div>
+                {mostrarFormulario && (
+                   <div className={`foverflow-y-auto flex flex-col gap-4 z-50 bg-white items-center w-[350px] max-w-full sm:w-[525px] p-10 m-auto shadow-lg mt-10 text-[#666666] rounded-xl relative`}>
+                      <div className='absolute left-4 top-2 cursor-pointer' onClick={() => setMostrarModal(!mostrarModal), () => setMostrarFormulario(!mostrarFormulario)}>
+                        <Icon icon="solar:arrow-left-linear" style={{ color: "#005261" }} width={40} />
+                      </div>
+                      <form  onSubmit={(e) => handleFormSubmit(e)} className=' space-y-8 sm:w-3/4 h-[700px] flex items-center flex-col z-50 bg-white '>
+                       <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                           Tipo de Recado
+                          {opcaoForm == 1 ?(  
+                            <input type="text" name="organizacaoSala" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Organização de Sala'}/>
+                            ):opcaoForm == 2?(
+                              <input type="text" name="campeonatoPatio" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Campeonato de Pátio'}/>
+                            ):opcaoForm == 3?(
+                              <input type="text" name="campeonatoQuadra" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Campeonato de Quadra'}/>
+                            ):opcaoForm == 4?(
+                            <input type="text" name="prazo" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Prazos'}/>
+                            ):opcaoForm == 5?(
+                            <input type="text" name="doacao" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Doações'}/>
+                           ):(
+                            <input type="text" name="oficina" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui' value={'Oficinas'}/>
+                           )
+                          }
+                        </label>
 
-      <div className={`${largura < 630 ? 'block' : 'hidden'}  w-[100px] flex justify-center `}>
-          <button className='w-[50px] h-[50px] bg-[#005261] grid place-content-center  rounded-2xl hover:scale-110 duration-150 transition-all' onClick={() => setMostrarCalendario(!mostrarCalendario)}>
-              <Image src={'/images/img-calendario.svg'} className="text-white" width={25} height={24}/>
-          </button>
-      </div>
-
+                        <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                          Titulo 
+                          <input type="text" name="titulo" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui'/>
+                        </label>
+                   
+                        <label className=' gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                           <input type="radio" name="visualizacao" className='bg-[#E6EFF0] text-[#005261]  text-lg' value="representante"/> Representantes <br/>
+                          <input type="radio" name="visualizacao" className='bg-[#E6EFF0] text-[#005261] text-lg' value="aluno"/> Todos os Alunos
+                        </label>
+                        <label className=' gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                         <input type="date" name="diaMarcado" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui'/>
+                        </label>
+                        <label className=' gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                         <input type="time" name="horarioRecado" className='bg-[#E6EFF0] text-[#005261] font-medium text-lg' placeholder='Digite aqui'/>
+                        </label>
+                        <label className='flex flex-col gap-3 w-full px-9 pt-3 pb-2 rounded-2xl bg-[#E6EFF0]'>
+                          Recado 
+                          <textarea name="recado" className='bg-[#E6EFF0] w-full text-[#005261] font-medium text-lg' placeholder='Digite aqui'/>
+                        </label>
+                        <div className='flex sm:flex-row justify-evenly w-full gap-2'>
+                          <button type='submit' className='w-1/2  rounded-2xl self-start bg-[#005261] text-white font-medium p-4'>Salvar</button>
+                          <button type='button' className='w-1/2  rounded-2xl self-start bg-[#005261] text-white font-medium p-4' onClick={() => setMostrarModal(!mostrarModal), () => setMostrarFormulario(!mostrarFormulario)}>
+                            Cancelar
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  )}
+            </div>
+          </>
+        )}
     </div>
   );
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -7,8 +7,15 @@ export default function Calendar() {
     const [primeiroDiaSemana, setPrimeiroDiaSemana] = useState(0);
     const [totalDiasMes, setTotalDiasMes] = useState(0);
 
-    const [totalDiasMesAnterior, setTotalDiasMesAnterior] = useState(new Date(anoSelecionado, mesSelecionado, 0).getDate())
+    const [totalDiasMesAnterior, setTotalDiasMesAnterior] = useState(new Date(anoSelecionado, mesSelecionado, 0).getDate());
 
+    const hoje = new Date();
+    const diaHoje = hoje.getDate();
+    const mesHoje = hoje.getMonth();
+    const anoHoje = hoje.getFullYear();
+
+    console.log(diaHoje,mesHoje,anoHoje)
+    
     useEffect(() => {
         const primeiroDia = new Date(anoSelecionado, mesSelecionado, 1);
         const mes = new Date(anoSelecionado, mesSelecionado + 1, 0);
@@ -33,9 +40,9 @@ export default function Calendar() {
     }
 
     return (
-        <div className='bg-white rounded-lg w-full p-8'>
-            <div className='text-[#005261] font-bold  flex justify-evenly mb-5'>
-                <select value={mesSelecionado} onChange={(e) => setMesSelecionado(e.target.value)}>
+        <div className='bg-white rounded-lg w-full p-4'>
+            <div className='text-[#005261] font-bold flex justify-evenly mb-5'>
+                <select value={mesSelecionado} onChange={(e) => setMesSelecionado(Number(e.target.value))}>
                     <option value={0}>Janeiro</option>
                     <option value={1}>Fevereiro</option>
                     <option value={2}>Março</option>
@@ -49,24 +56,66 @@ export default function Calendar() {
                     <option value={10}>Novembro</option>
                     <option value={11}>Dezembro</option>
                 </select>
-                <input type="number" value={anoSelecionado} className="text-[#005261] font-bold w-[100px]" onChange={(e) => setAnoSelecionado(e.target.value)} />
+                <input type="number" value={anoSelecionado} className="text-[#005261] font-bold w-[100px]" onChange={(e) => setAnoSelecionado(Number(e.target.value))} />
             </div>
-            <div className="grid grid-cols-7 gap-5">
+            <div className="grid grid-cols-7 gap-10 mr-5">
                 {renderDiasAnteriores().map((dia, index) => (
-                    <p key={index} className="text-gray-400">{dia}</p>
+                    <p key={index} className="text-gray-400 h-[30px] w-[30px] grid place-content-center -mt-2">{dia}</p>
                 ))}
                 {diasDoMes.map((dia) => (
-                    <p key={dia}>{dia}</p>
+                    <p key={dia} className={dia === diaHoje && mesSelecionado === mesHoje && anoSelecionado === anoHoje ? "h-[30px] w-[30px] -mt-2  grid place-content-center rounded-full   bg-[#005261] text-white" : "grid place-content-center -mt-2 h-[30px] w-[30px]"}>
+                        {dia}
+                    </p>
                 ))}
                 {renderDiasProximos().map((dia, index) => (
-                    <p key={index} className="text-gray-400">{dia}</p>
+                    <p key={index} className="text-gray-400  h-[30px] w-[30px] grid place-content-center -mt-2">{dia}</p>
                 ))}
             </div>
         </div>
     );
 }
 
-/*
-new Date(2024, 0, 1) -> 2024 JANEIRO 1
-new Date(2024, 1, 1) -> 2024 FEVEREIRO 1
-*/
+
+
+    
+    
+{/*
+    
+
+    <div className='bg-white rounded-lg w-full p-4'>
+            <div className='text-[#005261] font-bold flex justify-evenly mb-5'>
+                <select value={mesSelecionado} onChange={(e) => setMesSelecionado(Number(e.target.value))}>
+                    <option value={0}>Janeiro</option>
+                    <option value={1}>Fevereiro</option>
+                    <option value={2}>Março</option>
+                    <option value={3}>Abril</option>
+                    <option value={4}>Maio</option>
+                    <option value={5}>Junho</option>
+                    <option value={6}>Julho</option>
+                    <option value={7}>Agosto</option>
+                    <option value={8}>Setembro</option>
+                    <option value={9}>Outubro</option>
+                    <option value={10}>Novembro</option>
+                    <option value={11}>Dezembro</option>
+                </select>
+                <input type="number" value={anoSelecionado} className="text-[#005261] font-bold w-[100px]" onChange={(e) => setAnoSelecionado(Number(e.target.value))} />
+            </div>
+            <div className="grid grid-cols-7 gap-10 mr-5">
+                {renderDiasAnteriores().map((dia, index) => (
+                    <p key={index} className="text-gray-400 h-[30px] w-[30px] grid place-content-center -mt-2">{dia}</p>
+                ))}
+                {diasDoMes.map((dia) => (
+                    <p key={dia} className={dia ===  && mesSelecionado === mesHoje && anoSelecionado === anoHoje ? "h-[30px] w-[30px] -mt-2  grid place-content-center	  rounded-full   bg-blue-500 text-white" : ""}>
+                        {dia}
+                    </p>
+                ))}
+                {renderDiasProximos().map((dia, index) => (
+                    <p key={index} className="text-gray-400  h-[30px] w-[30px] grid place-content-center -mt-2">{dia}</p>
+                ))}
+            </div>
+        </div>
+    
+
+*/}
+    
+    
