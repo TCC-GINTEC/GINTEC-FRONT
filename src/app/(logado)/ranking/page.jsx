977d4f0947898d9005db00e615d4fbb5226c8ced
dryelleebelin <dryellesilva07@gmail.com>
2024-11-07 03:33:37 -0300

    fix: layout tela de pontuação geral

--- src/app/(logado)/ranking/page.jsx
+++ src/app/(logado)/ranking/page.jsx
@@ -1,19 +1,15 @@
-"use client";
-
-import Placed from '@/components/Ranking/Placed';
+"use client"
+import Placed from '@/components/Ranking/Placed'
 import Scores from "@/components/Ranking/Scores"
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import httpClient from "@/service/api"
-import { toast } from 'sonner';
+import { toast } from 'sonner'
+import './style.css'
 
 export default function Ranking() {
-  const [selectRanking, setSelectRanking] = useState("Aluno");
-  const [seachDate, setSeachDate] = useState("");
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    handleGetPlayers();
-  }, [selectRanking])
+  const [selectRanking, setSelectRanking] = useState("Aluno")
+  const [seachDate, setSeachDate] = useState("")
+  const [data, setData] = useState([])
 
   const handleGetPlayers = async () => {
     try {
@@ -22,33 +18,33 @@ export default function Ranking() {
           httpClient.post("/Sala/Ranking").then((response) => {
             setData((response.data?.map((item) => {
               return {
-                  foto: item.fotoSala,
-                  descricao: item.descricao,
-                  pontuacao: item.pontuacao
+                foto: item.fotoSala,
+                descricao: item.descricao,
+                pontuacao: item.pontuacao
               }
-          }) ?? []))
+            }) ?? []))
           })
-          break;
+          break
         case 'Padrinho':
           httpClient.get("/Usuario/ObterPontuacaoGeral?id=Padrinho").then((response) => {
             setData((response.data?.filter(p => p.isPadrinho).map((item) => {
               return ({
-                  foto: item.fotoPerfil,
-                  descricao: item.nome,
-                  pontuacao: item.pontos.pontuacaGeral
+                foto: item.fotoPerfil,
+                descricao: item.nome,
+                pontuacao: item.pontos.pontuacaGeral
               })
-          }) ?? []).sort((a, b) => b.pontuacao - a.pontuacao))
+            }) ?? []).sort((a, b) => b.pontuacao - a.pontuacao))
           })
-          break;
-        case 'Aluno':          
+          break
+        case 'Aluno':
           httpClient.get("/Usuario/ObterPontuacaoGeral").then((response) => {
             setData((response.data?.filter(p => !p.isPadrinho).map((item) => {
               return ({
-                  foto: item.fotoPerfil,
-                  descricao: item.nome,
-                  pontuacao: item.pontos.pontuacaGeral
+                foto: item.fotoPerfil,
+                descricao: item.nome,
+                pontuacao: item.pontos.pontuacaGeral
               })
-          }) ?? []).sort((a, b) => b.pontuacao - a.pontuacao))
+            }) ?? []).sort((a, b) => b.pontuacao - a.pontuacao))
           })
           break
       }
@@ -57,27 +53,36 @@ export default function Ranking() {
       toast.error("Algo deu errado!")
     }
   }
+
+  useEffect(() => {
+    handleGetPlayers()
+  }, [selectRanking])
+
   return (
-    <>
-      <h1 className="text-[32px] font-[600]">Pontuação Geral</h1>
-      <p className="text-[#666666]">Ranking geral</p>
-      <div className='flex sm:justify-end flex-col sm:flex-row gap-4 w-3/4 p-4'>
+    <main className='container-ranking'>
+      <div className='header'>
+        <div className='header-l'>
+          <h1>Pontuação Geral</h1>
+          <p>Ranking geral</p>
+        </div>
 
-
-        <select className="bg-slate-200 sm:w-1/2 w-full rounded-lg p-2" value={selectRanking} onChange={(evt) => { setSelectRanking(evt.target.value) }}>
-          <option value="Aluno">Melhores jogadores</option>
-          <option value="Padrinho">Melhores padrinhos</option>
-          <option value="Sala">Melhores Salas</option>
-        </select>
+        <div className='header-r'>
+          <select className="bg-slate-200 sm:w-1/2 w-full rounded-lg p-2" value={selectRanking} onChange={(evt) => { setSelectRanking(evt.target.value) }}>
+            <option value="Aluno">Melhores jogadores</option>
+            <option value="Padrinho">Melhores padrinhos</option>
+            <option value="Sala">Melhores Salas</option>
+          </select>
+        </div>
       </div>
-      <div className="bg-[url('/images/fundoPontuacaoGeral.svg')] w-3/4 bg-cover rounded-xl sm:w-full mb-5 p-3 flex items-center gap-5 flex-col sm:flex-row">
+
+      <div className="top-ranking">
         {/* os 3 pontuadores */}
-        <Placed users={data.slice(0, 3)} />
+        <Placed users={data.slice(0, 3)} /> 
       </div>
-      <div className={`border w-full `}>
-        {/* pontuadores em geral -1 div de base feita*/}
+
+      <div className='scores'>
         <Scores users={data} />
       </div>
-    </>
+    </main>
   )
 }
--- src/components/Ranking/Scores.jsx
+++ src/components/Ranking/Scores.jsx
@@ -3,40 +3,35 @@ export default function Scores({ users }) {
   return (
     users.map((user, index) => {
       return (
-        <>
-          <div className='mb-4 p-4 border rounded-2xl bg-gray-100
-              flex flex-col gap-3'>
-            <div className='flex flex-col gap-4 items-center'>
-              {<span className='text-white py-2 w-full text-center text-2xl font-semibold bg-slate-400 rounded-2xl'>
-                {index + 1}º Lugar
-              </span>
-              }
-              <img alt="" src={user.foto ? user.foto : "https://cdn-icons-png.flaticon.com/512/3106/3106921.png"} className='m-auto rounded-full' height={150} width={150} />
-              <span className='text-2xl font-semibold'>{user.nome}</span>
-            </div>
-            <div className='flex flex-col gap-3'>
-              {index != 0 ?
-                <div>
-                  <h3 className='font-bold uppercase mb-2'>Diferença pro {index}º lugar</h3>
-                  <span>{users[index - 1].pontuacao - user.pontuacao == 1 ?
-                    `${users[index - 1].pontuacao - user.pontuacao} ponto`
-                    :
-                    `${users[index - 1].pontuacao - user.pontuacao} pontos`}</span>
-                </div>
-                :
-                ""
-              }
+        <div className='container-score mb-4 p-4 border rounded-2xl bg-gray-100 flex flex-col gap-3'>
+          <div className='container-score-l'>
+            <span className='lugar'>{index + 1}</span>
+            <img alt="Imagem" src={user.foto ? user.foto : "https://cdn-icons-png.flaticon.com/512/3106/3106921.png"}/>
+            <span className='text'>{user.descricao}</span>
+          </div>
 
-              <div className='flex flex-col gap-3'>
-                <div className='flex gap-3'>
-                  <h3 className='font-bold uppercase'>Pontuação</h3>
-                  <span>{user.pontuacao}</span>
-                </div>
+          <div className='container-score-r'>
+            {index != 0 ?
+              <div>
+                <h3 className='font-bold mb-2'>Diferença pro {index}º lugar</h3>
+                <span>{users[index - 1].pontuacao - user.pontuacao == 1 ?
+                  `${users[index - 1].pontuacao - user.pontuacao} ponto`
+                  :
+                  `${users[index - 1].pontuacao - user.pontuacao} pontos`}</span>
+              </div>
+              :
+              ""
+            }
+
+            <div className='flex flex-col gap-3'>
+              <div className='flexx flex gap-3'>
+                <h3 className='font-bold'>Pontuação final</h3>
+                <span>{user.pontuacao}</span>
               </div>
             </div>
           </div>
-        </>)
+        </div>
+      )
     })
-
-  );
+  )
 }
