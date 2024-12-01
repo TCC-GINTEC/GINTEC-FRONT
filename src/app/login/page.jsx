e004fb7d47089d0f2ffd01462ab04ab3e5cec837
Nathan Silva <nathan.silva@ndevs.com.br>
2024-12-01 19:07:33 -0300

    fix text login screen

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -64,7 +64,7 @@ export default function Login() {
                         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
 
                             <h2 className="mt-10  text-3xl font-semibold leading-9 tracking-tight text-black mb-7">Faça seu login</h2>
-                            <p className='text-2x 1 text-[#b7b7b7]'>Digite seu Email institucional ou o seu RM nos campos abaixo e clique em entrar para fazer seu login</p>
+                            <p className='text-2x 1 text-[#b7b7b7]'>Digite seu RM e o sua senha nos campos abaixo e clique em entrar para fazer o seu login</p>
                         </div>
 
                         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
